97d7b7a98e0da20a798fc317f94d3a7ccae8b2e5
e-schultz <=>
2022-11-02 13:57:57 -0400

    prettier

--- adobe-ourwork-aem-dev/components/BarChart/BarChart.stories.tsx
+++ adobe-ourwork-aem-dev/components/BarChart/BarChart.stories.tsx
@@ -1,33 +1,33 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { BarChart } from './BarChart';
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { BarChart } from './BarChart'
 
 export default {
-    title: 'Example/BarChart',
-    component: BarChart
-  } as ComponentMeta<typeof BarChart>;
+  title: 'Example/BarChart',
+  component: BarChart
+} as ComponentMeta<typeof BarChart>
 
-const Template: ComponentStory<typeof BarChart> = (args) => BarChart(args);
+const Template: ComponentStory<typeof BarChart> = (args) => BarChart(args)
 
 export const WithAxes = Template.bind({})
 WithAxes.args = {
-    colours: ['rgb(231, 96, 12)'],
-    labels: ['Girls', 'Boys', 'Women', 'Men'],
-    data: [480000, 410000, 250000, 220000],
-    withAxes: true,
-    yStepSize: 100000,
-    aspectRatio: 0.8,
-    topBarLabels: false,
+  colours: ['rgb(231, 96, 12)'],
+  labels: ['Girls', 'Boys', 'Women', 'Men'],
+  data: [480000, 410000, 250000, 220000],
+  withAxes: true,
+  yStepSize: 100000,
+  aspectRatio: 0.8,
+  topBarLabels: false
 }
 
 export const WithoutAxesWithIndvTopBarLabel = Template.bind({})
 WithoutAxesWithIndvTopBarLabel.args = {
-    colours: ['rgb(231, 96, 12)', 'rgb(255, 166, 102)', 'rgb(255, 225, 204)' ],
-    labels: ['2021', '2013'],
-    data: ['83.5%', '74.1%'],
-    withAxes: false,
-    yStepSize: 10,
-    aspectRatio: 0.5,
-    topBarLabels: true,
-    bottomLabelTitle: 'Reading Comprehension',
-    bottomLabelSubtitle: 'Eravur Pattu, Sri Lanka'
+  colours: ['rgb(231, 96, 12)', 'rgb(255, 166, 102)', 'rgb(255, 225, 204)'],
+  labels: ['2021', '2013'],
+  data: ['83.5%', '74.1%'],
+  withAxes: false,
+  yStepSize: 10,
+  aspectRatio: 0.5,
+  topBarLabels: true,
+  bottomLabelTitle: 'Reading Comprehension',
+  bottomLabelSubtitle: 'Eravur Pattu, Sri Lanka'
 }
--- adobe-ourwork-aem-dev/utils/convertNumbers.js
+++ adobe-ourwork-aem-dev/utils/convertNumbers.js
@@ -2,11 +2,11 @@
 // toFixed(d) returns a string that has exactly 'd' digits
 // after the decimal place, rounding if necessary.
 export function numFormatter(num) {
-    if(num > 999 && num < 1000000){
-        return (num/1000).toFixed(0) + 'k'; // convert to k for number from > 1000 < 1 million 
-    }else if(num > 1000000){
-        return (num/1000000).toFixed(0) + 'm'; // convert to m for number from > 1 million 
-    }else if(num < 900){
-        return num; // if value < 1000, nothing to do
-    }
+  if (num > 999 && num < 1000000) {
+    return (num / 1000).toFixed(0) + 'k' // convert to k for number from > 1000 < 1 million
+  } else if (num > 1000000) {
+    return (num / 1000000).toFixed(0) + 'm' // convert to m for number from > 1 million
+  } else if (num < 900) {
+    return num // if value < 1000, nothing to do
+  }
 }
