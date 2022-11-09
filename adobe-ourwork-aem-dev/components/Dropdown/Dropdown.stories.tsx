56e4c0e4d1cc84f28bcd7b3bb2a05a3f86899e06
Cher G <cgaringan@jetblue.com>
2022-11-09 14:11:11 -0500

    Dropdown component

---
+++ adobe-ourwork-aem-dev/components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import Dropdown, { DropdownProps } from './Dropdown'
+
+export default {
+  title: 'Example/Dropdown',
+  component: Dropdown
+} as ComponentMeta<typeof Dropdown>
+
+const Template: ComponentStory<DropdownProps> = (args: DropdownProps) => (
+  <div
+    style={{
+      background: args.isDark ? '#333333' : '#f5f5f5',
+      padding: '24px 32px'
+    }}
+  >
+    <Dropdown {...args} />
+  </div>
+)
+
+const defaultArgs: DropdownProps = {
+  dropdownLabel: 'Dropdown Label',
+  id: 'selectId',
+  isDark: false,
+  options: [
+    {
+      label: 'Option 1',
+      value: 'option 1'
+    },
+    {
+      label: 'Option 2',
+      value: 'option 2'
+    },
+    {
+      label: 'Option 3',
+      value: 'option 3'
+    },
+    {
+        label: 'Option 4',
+        value: 'option 4'
+      }
+  ],
+  updateSelection: (value: string) => {
+    alert(`Value chosen: ${value}`)
+  }
+}
+
+export const Default = Template.bind({})
+Default.args = defaultArgs
---
+++ adobe-ourwork-aem-dev/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './Dropdown.scss'
+
+interface Option {
+  value: string
+  label: string
+}
+
+export interface DropdownProps {
+  dropdownLabel: string
+  id: string
+  isDark: boolean
+  options: Option[]
+  updateSelection: (value: string) => void
+}
+
+const Dropdown = ({
+  dropdownLabel,
+  id,
+  isDark,
+  options,
+  updateSelection
+}: DropdownProps) => {
+  return (
+    <div className={`select-dropdown${isDark ? '--darkTheme' : ''}`}>
+      <div>
+        <label htmlFor={id} className='select-dropdown__label'>
+          {dropdownLabel}
+        </label>
+      </div>
+      <div>
+        <select
+          className='select-dropdown__select'
+          id={id}
+          onChange={(e) => {
+            return updateSelection(e.target.value)
+          }}
+        >
+          {options.map((option) => {
+            return (
+              <option className='select-dropdown__option' value={option.value}>
+                {option.label}
+              </option>
+            )
+          })}
+          )
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default Dropdown
