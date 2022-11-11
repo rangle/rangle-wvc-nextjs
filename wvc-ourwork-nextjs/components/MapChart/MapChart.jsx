5037c3abf7021b7746a221b9f4dde570efa27351
e-schultz <=>
2022-11-11 18:19:16 -0500

    initial setup for chats

--- wvc-ourwork-nextjs/components/MapChart/MapChart.jsx
+++ wvc-ourwork-nextjs/components/MapChart/MapChart.jsx
@@ -1,13 +1,28 @@
-import React, { useRef, useMemo, useState } from 'react'
+import React, { useRef, useMemo, useState, useCallback } from 'react'
 import { Map, Source, Layer } from 'react-map-gl'
 import './MapChart.scss'
-
 
 // TODO - move to env and refresh token
 const TOKEN =
   'pk.eyJ1IjoiZS1zY2h1bHR6IiwiYSI6ImNsYTJ2MGYzdjA2Yzkzb3BhY2FlaGM1dDEifQ.HBoblCqe9VRjFOfUNacejg'
 
-function MapChartGL({interactive = true,}) {
+function MapChartGL({ interactive = true, children, zoom = 0, ...props }) {
+  const [x,setX] = useState(false);
+  const [viewState, setViewState] = useState({
+    
+    // west, siyth, east, north
+    /*bounds: [
+      73.61720299999996, 18.168884000000048, 134.77359000000013,
+      53.55443600000007
+    ],*/
+  })
+  const onToggle = useCallback((evt)=>{
+    console.log(mapRef)
+    setX((v)=>!v)
+    //mapRef.current.scrollZoom.disable();
+    mapRef.current.setZoom(zoom)
+  },[])
+  console.log({interactive,zoom})
   const geojson = {
     type: 'FeatureCollection',
     features: [
@@ -36,24 +51,29 @@ function MapChartGL({interactive = true,}) {
   }
 
   const mapRef = useRef(null)
-
+  if(mapRef.current) {
+    //mapRef.current.setZoom(zoom)
+  }
   // https://gist.github.com/graydon/11198540
 
   return (
-    <Map
-      interactive={false}
-      mapboxAccessToken={TOKEN}
-      initialViewState={{
-        // west, siyth, east, north
-        bounds: [
-          73.61720299999996, 18.168884000000048, 134.77359000000013,
-          53.55443600000007
-        ],
-        zoom: 0
-      }}
-      mapStyle='mapbox://styles/mapbox/dark-v10'
-    >
-      
+    <>
+    
+    <button onClick={onToggle}>Toggle! - {x ? 'yes' : 'no'}</button>
+      <Map
+        
+        mapboxAccessToken={TOKEN}
+        zoom={zoom}  
+        interactive={interactive}
+        scrollZoom={interactive}
+        dragPan={interactive}
+        keyboard={interactive}
+        cursor={interactive ? 'pointer' : 'auto'}
+        mapStyle='mapbox://styles/mapbox/dark-v10'
+      >
+        {children}
+
+        {/*
       <Source
         id='my-data-ch'
         type='geojson'
@@ -67,9 +87,10 @@ function MapChartGL({interactive = true,}) {
         data='https://inmagik.github.io/world-countries/countries/AFG.geojson'
       >
         <Layer {...layerStyle2} />
-      </Source>
-
-    </Map>
+    
+    </Source>*/}
+      </Map>
+    </>
   )
 }
 
--- wvc-ourwork-nextjs/components/MapChart/MapChart.stories.jsx
+++ wvc-ourwork-nextjs/components/MapChart/MapChart.stories.jsx
@@ -1,12 +1,22 @@
 import MapChart from './MapChart'
+import { Source, Layer } from 'react-map-gl'
 import { useState } from 'react'
 
 export default {
   title: 'Example/MapChart',
   component: MapChart
 }
-
+const layerStyle1 = {
+  id: 'point1',
+  type: 'line',
+  paint: {
+    'line-width': 2,
+    'line-color': '#0080ef'
+  }
+}
 const Template = (args) => {
+  const [x, setX] = useState(true);
+
   return (
     <div
       style={{
@@ -18,10 +28,23 @@ const Template = (args) => {
         overflow: 'hidden'
       }}
     >
-      <MapChart />
+     
+     
+      <MapChart {...args}>
+        <Source
+          id='my-data-ch'
+          type='geojson'
+          data='https://inmagik.github.io/world-countries/countries/CHN.geojson'
+        >
+          <Layer {...layerStyle1} />
+        </Source>
+      </MapChart>
     </div>
   )
 }
 
 export const SimpleMap = Template.bind({})
-SimpleMap.args = {}
+SimpleMap.args = {
+  interactive: true,
+  zoom: 0,
+}
