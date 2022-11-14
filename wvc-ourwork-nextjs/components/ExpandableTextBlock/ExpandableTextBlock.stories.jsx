d2d294cbf812cc58fadf751e4fc9d676f11d9997
Scott Fuller <44618719+scottfuller4@users.noreply.github.com>
2022-11-14 11:55:53 -0500

    add expandable text block component

---
+++ wvc-ourwork-nextjs/components/ExpandableTextBlock/ExpandableTextBlock.stories.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import ExpandableTextBlock from './ExpandableTextBlock'
+
+export default {
+  title: 'Example/ExpandableTextBlock',
+  component: ExpandableTextBlock
+}
+
+const Template = (args) => <ExpandableTextBlock {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc massa nullam nunc ac vel justo scelerisque. Ipsum eget aliquam non adipiscing odio ornare in. Sed feugiat ultricies adipiscing nisl pellentesque elementum tortor massa sit. Tellus arcu facilisis turpis fermentum libero vulputate mauris amet sit. Ac tortor suspendisse aliquam volutpat dolor eget arcu. Sed quis vitae leo mi nisl id et. Sed interdum eget lacus interdum tincidunt duis orci enim.',
+  footnote: 'Lorem ipsum footnote',
+  title: 'Early childhood development'
+}
