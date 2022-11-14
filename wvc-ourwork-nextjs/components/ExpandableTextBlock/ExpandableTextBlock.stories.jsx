8e863a0ad07103e0e49cd2bfdf5601039cfa6dd8
Scott Fuller <44618719+scottfuller4@users.noreply.github.com>
2022-11-14 12:00:32 -0500

    add story for simple variant

--- wvc-ourwork-nextjs/components/ExpandableTextBlock/ExpandableTextBlock.stories.jsx
+++ wvc-ourwork-nextjs/components/ExpandableTextBlock/ExpandableTextBlock.stories.jsx
@@ -15,3 +15,8 @@ Default.args = {
   footnote: 'Lorem ipsum footnote',
   title: 'Early childhood development'
 }
+
+export const BodyOnly = Template.bind({})
+BodyOnly.args = {
+  body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc massa nullam nunc ac vel justo scelerisque. Ipsum eget aliquam non adipiscing odio ornare in. Sed feugiat ultricies adipiscing nisl pellentesque elementum tortor massa sit. Tellus arcu facilisis turpis fermentum libero vulputate mauris amet sit. Ac tortor suspendisse aliquam volutpat dolor eget arcu. Sed quis vitae leo mi nisl id et. Sed interdum eget lacus interdum tincidunt duis orci enim.'
+}
