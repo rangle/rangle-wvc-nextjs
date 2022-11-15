760b04731b29a2dc0606ed65526ae1afb962f4bb
Cher G <cgaringan@jetblue.com>
2022-11-15 12:46:57 -0500

    add comments to title and subtitle color changes on the bar chart

--- wvc-ourwork-nextjs/components/Charts/BarChart/BarChart.tsx
+++ wvc-ourwork-nextjs/components/Charts/BarChart/BarChart.tsx
@@ -51,9 +51,11 @@ export function BarChart({
         position: titlePosition,
         color:
           titlePosition === 'top'
+          // if the title is on the top, just change the darkmode color
             ? !isDarkMode
               ? ''
               : '#ffffff'
+          // if title is on the bottom, this becomes subtitle, so change colors to match subtitle
             : !isDarkMode
             ? '#666666'
             : '#cccccc',
@@ -68,9 +70,11 @@ export function BarChart({
         position: titlePosition,
         color:
           titlePosition === 'top'
+          // if the subtitle is on the top, just change the darkmode color
             ? !isDarkMode
               ? ''
               : '#cccccc'
+          // if subtitle is on the bottom, this becomes the title, so change colors to match title
             : !isDarkMode
             ? '#333333'
             : '#ffffff',
