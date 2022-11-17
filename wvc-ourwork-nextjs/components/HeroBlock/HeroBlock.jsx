a1c4ae47224c0432534f3ab20e338a4adb76c307
Scott Fuller <44618719+scottfuller4@users.noreply.github.com>
2022-11-17 16:23:34 -0500

    finish sector template layout

---
+++ wvc-ourwork-nextjs/components/HeroBlock/HeroBlock.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+import './HeroBlock.scss'
+
+import HeroCountry from '../../assets/HeroCountry.png'
+import HeroSector from '../../assets/HeroSector.png'
+import HeroProgram from '../../assets/HeroProgram.png'
+
+import Button from '../Button/Button'
+import SummaryBlock from '../SummaryBlock/SummaryBlock'
+
+const HeroBlock = ({
+  body,
+  children,
+  ctaLabel,
+  ctaUrl,
+  highlights,
+  page,
+  sectorImgAlt,
+  sectorImgSrc,
+  title
+}) => {
+  const [imgSrc, setImgSrc] = useState(HeroProgram)
+
+  useEffect(() => {
+    if (page === 'sector') {
+      setImgSrc(HeroSector)
+    } else if (page === 'country') {
+      setImgSrc(HeroCountry)
+    } else if (page === 'program') {
+      setImgSrc(HeroProgram)
+    }
+  }, [[page]])
+
+  return (
+    <div className='hero-block'>
+      <div className='hero-block__hero-container'>
+        <div className='hero-block__top-container'>
+          {page === 'sector' && sectorImgSrc && (
+            <img
+              src={sectorImgSrc}
+              alt={sectorImgAlt}
+              className='hero-block__sector-image'
+            />
+          )}
+        </div>
+        <div className='hero-block__background-img'>
+          <img src={imgSrc} alt='' aria-hidden />
+          <div className='hero-block__hero-content'>
+            <h1 className='hero-block__title'>{title}</h1>
+            {body && <p className='hero-block__body'>{body}</p>}
+            <Button label={ctaLabel} url={ctaUrl} />
+          </div>
+        </div>
+      </div>
+      <div className='hero-block__summary-container'>
+        <div className='hero-block__summary-block'>
+          <SummaryBlock
+            highlights={highlights}
+            order={page === 'sector' ? 'reverse' : 'title-first'}
+          />
+        </div>
+        <div className='hero-block__children-container'>{children}</div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBlock
--- wvc-ourwork-nextjs/components/SummaryBlock/SummaryBlock.jsx
+++ wvc-ourwork-nextjs/components/SummaryBlock/SummaryBlock.jsx
@@ -1,12 +1,19 @@
 import Tooltip from '../Tooltip/Tooltip'
 import './SummaryBlock.scss'
 
-const SummaryBlock = ({ direction = 'horizontal', highlights }) => {
+const SummaryBlock = ({
+  direction = 'horizontal',
+  highlights,
+  order = 'title-first'
+}) => {
   return (
     <div className={`summary-block summary-block--${direction}`}>
       {highlights.map((highlight) => {
         return (
-          <div key={highlight.value} className='summary-block__highlight'>
+          <div
+            key={highlight.value}
+            className={`summary-block__highlight summary-block__highlight--${order}`}
+          >
             <div className='summary-block__highlight-title-container'>
               <p className='summary-block__highlight--title'>
                 {highlight.title}
