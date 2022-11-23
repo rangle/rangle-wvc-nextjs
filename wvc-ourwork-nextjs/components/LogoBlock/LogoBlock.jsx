015d01dd3fed72d4f91cdc30c9dc48da2dd5fff3
Nancy Du <nancy.du@rangle.io>
2022-11-23 10:21:58 -0500

    Tweak

--- wvc-ourwork-nextjs/components/LogoBlock/LogoBlock.jsx
+++ wvc-ourwork-nextjs/components/LogoBlock/LogoBlock.jsx
@@ -1,16 +1,16 @@
 import styles from './LogoBlock.module.scss'
 
-const LogoBlock = ({ ctaLabel, ctaUrl, logos, title }) => {
-  const ctaArrow = (
-    <svg xmlns='http://www.w3.org/2000/svg' width='8' height='12'>
-      <path
-        fill-rule='evenodd'
-        d='M4.823 6 0 1.487 1.589 0 8 6l-6.411 6L0 10.513 4.823 6Z'
-        clip-rule='evenodd'
-      />
-    </svg>
-  )
+const ctaArrow = (
+  <svg xmlns='http://www.w3.org/2000/svg' width='8' height='12'>
+    <path
+      fill-rule='evenodd'
+      d='M4.823 6 0 1.487 1.589 0 8 6l-6.411 6L0 10.513 4.823 6Z'
+      clip-rule='evenodd'
+    />
+  </svg>
+)
 
+const LogoBlock = ({ ctaLabel, ctaUrl, logos, title }) => {
   return (
     <div className={styles['logo-block']}>
       <h3 className={styles['logo-block__title']}>{title}</h3>
