edd067297e33824112dd0f4b2a6fb8a957efeb36
Nancy Du <nancy.du@rangle.io>
2022-12-02 13:38:07 -0500

    Rolling credits on gradient, map at end.

--- components/Homepage/RollingCreditsMap/RollingCreditsMap.jsx
+++ components/Homepage/RollingCreditsMap/RollingCreditsMap.jsx
@@ -1,55 +1,14 @@
 import styles from './RollingCreditsMap.module.scss'
 
-import { animate, useInView } from 'framer-motion'
-import React, { useEffect, useState, useRef } from 'react'
+import React from 'react'
 import MapHeaderContainer from '../../MapChart/MapChartHeader/MapHeaderContainer'
 
-const RollingCreditsMap = ({ imageAlt, imageSrc, mapCreditsData }) => {
+const RollingCreditsMap = ({ mapCreditsData }) => {
   return (
-    // MAP ON ITS OWN...
-    // <div>
-    //   <MapHeaderContainer
-    //     className={styles['rolling-credits-map__bg-image']} // may not need this class
-    //     showHeaderControls={true} // should be false until rolling credits end
-    //     showMarkers={true} // should be false until rolling credits end
-    //     isDark={true}
-    //     duration={1000}
-    //     padding={40}
-    //     showEmptyPrograms={false}
-    //     legendText={'Explore our work around the world'}
-    //     countryDropdownLabel={'Countries'}
-    //     programDropdownLabel={'Program Type'}
-    //     interactive={false}
-    //   />
-    // </div>
-
     <div className={styles['rolling-credits-map']}>
       <div className={styles['rolling-credits-map__relative-wrapper']}>
         <div className={styles['rolling-credits-map__bg-outer']}>
-          <div className={styles['rolling-credits-map__bg-inner']}>
-            {/* position fixed background image -- can we treat the map the same? */}
-            {/* <img
-              className={styles['rolling-credits-map__bg-image']}
-              src={imageSrc}
-              alt=''
-            /> */}
-            <div>
-              <MapHeaderContainer
-                // className={styles['rolling-credits-map__bg-image']}  // may not need this class
-                showHeaderControls={true} // should be false until rolling credits end
-                showMarkers={false} // should be false until rolling credits end
-                isDark={true}
-                duration={1000}
-                padding={40}
-                showEmptyPrograms={false}
-                legendText={'Explore our work around the world'}
-                countryDropdownLabel={'Countries'}
-                programDropdownLabel={'Program Type'}
-                interactive={false}
-                zoom={1}
-              />
-            </div>
-          </div>
+          <div className={styles['rolling-credits-map__bg-inner']}></div>
         </div>
         <div>
           {mapCreditsData &&
@@ -67,7 +26,21 @@ const RollingCreditsMap = ({ imageAlt, imageSrc, mapCreditsData }) => {
             })}
         </div>
       </div>
-      <div> map goes here</div>
+      <div>
+        <MapHeaderContainer
+          showHeaderControls={true}
+          showMarkers={false}
+          isDark={true}
+          duration={1000}
+          padding={40}
+          showEmptyPrograms={false}
+          legendText={'Explore our work around the world'}
+          countryDropdownLabel={'Countries'}
+          programDropdownLabel={'Program Type'}
+          interactive={false}
+          zoom={1.25}
+        />
+      </div>
     </div>
   )
 }
