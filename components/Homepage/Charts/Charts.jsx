a70685acbed35f795512247c7e0cd8406dd9e6e5
Cher G <cgaringan@jetblue.com>
2022-12-08 23:41:54 -0500

    removed main page charts

--- components/Homepage/Charts/Charts.jsx
+++ components/Homepage/Charts/Charts.jsx
@@ -1,13 +1,30 @@
 import styles from './Charts.module.scss'
+import { getGraph } from '../../../utils/getGraphs'
+import { HomepageDoughnutChart } from '../../Charts/HomepageDoughnut/HomepageDoughnut'
 
-const Charts = () => {
+const Charts = ({ sectorData }) => {
+  const colors = sectorData?.map((item) => item.color)
+  const labels = sectorData?.map((item) => item.title)
+  const data = sectorData?.map((item) => item.percentage)
+
   return (
     <div className={styles['charts-container']}>
       <div className={styles['title']}>
         <h>How donations were invested in 2021</h>
       </div>
 
-      <div className={styles['section-title']}> program expenditure by sector</div>
+      <div className={styles['section-title']}>
+        Program expenditure by sector
+      </div>
+      {sectorData && sectorData.length && (
+        <HomepageDoughnutChart
+          displayLegend={true}
+          colors={colors}
+          labels={labels}
+          data={data}
+          ariaLabel={'Doughnut chart for sector overview'}
+        />
+      )}
     </div>
   )
 }
