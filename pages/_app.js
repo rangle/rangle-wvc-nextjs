7a549c9f26c6b494dbb5965e19b8d7fab85cc9df
Cher G <cgaringan@jetblue.com>
2022-12-09 19:32:34 -0500

    add google tag manager and custom charts dropdown event

--- pages/_app.js
+++ pages/_app.js
@@ -52,6 +52,30 @@ function MyApp({ Component, pageProps }) {
       <Script src='https://unpkg.com/@world-vision-canada/wvc-design-system@1.*.*/dist/wvc.umd.min.js'></Script>
       <Script src='https://sdks.shopifycdn.com/js-buy-sdk/1.11.0/index.umd.min.js'></Script>
 
+      {/* Google Tag Manager */}
+      <Script id='google-tag-manager' strategy='afterInteractive'>
+        {`
+        (function(w,d,s,l,i){
+          w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=1Ou-iTkObqWSPRcNVc5FJw&gtm_preview=env-13&gtm_cookies_win=x';
+          f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5F3MMBF');
+      `}
+      </Script>
+
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5F3MMBF&gtm_auth=1Ou-iTkObqWSPRcNVc5FJw&gtm_preview=env-13&gtm_cookies_win=x"
+            height="0" 
+            width="0" 
+            style="display:none;visibility:hidden"
+            ></iframe>`
+        }}
+      />
+      {/* End of Google Tag Manager */}
+
       <GlobalNavBar />
       <SubNavigation navItems={pageProps.navigation} />
       <Component {...pageProps} />
