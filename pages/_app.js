50a4a0e14d41073343da9873e64f8db123efd8bb
Nancy Du <nancyduyi@gmail.com>
2022-12-14 11:22:25 -0500

    fix prod analytics

--- pages/_app.js
+++ pages/_app.js
@@ -62,7 +62,7 @@ function MyApp({ Component, pageProps }) {
 
       {/* Google Tag Manager */}
       <Script id='google-tag-manager' strategy='afterInteractive'>
-        {process.env.BUILD_ENV === 'prod'
+        {process.env.NEXT_BUILD_ENV === 'prod'
           ? `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -81,7 +81,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <noscript
         dangerouslySetInnerHTML={{
           __html:
-            process.env.BUILD_ENV === 'prod'
+            process.env.NEXT_BUILD_ENV === 'prod'
               ? `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5F3MMBF"
           height="0" width="0" style="display:none;visibility:hidden"></iframe>`
               : `<iframe 
